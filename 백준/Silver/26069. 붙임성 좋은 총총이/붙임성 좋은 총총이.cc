4b9cd5c67962b47171c5385d1809f70986333e4d
ssohy <109409651+ssohy@users.noreply.github.com>
2026-01-07 19:01:24 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cc
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.cc
@@ -1,44 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <set>
 using namespace std;
-vector<string> dancing;
-int result = 1;
 
-void isDancing(string name1, string name2)
-{
-  bool isExist1 = false, isExist2 = false;
-  for(int j = 0; j < dancing.size(); j++){
-    if(name1 == dancing[j]){
-      isExist1 = true;
-    }
-    if(name2 == dancing[j]){
-      isExist2 = true;
-    }
-    if(isExist1 && isExist2) break;
-  }
-  if(isExist1 == false && isExist2 == true){
-    result++;
-    dancing.push_back(name1);
-  }
-  else if(isExist1 == true && isExist2 == false){
-    result++;
-    dancing.push_back(name2);
-  }
-}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-
-int main() 
-{
-    int N = 0;
-    string p1 = "", p2 = "";
+    int N;
     cin >> N;
-    
-    dancing.push_back("ChongChong");
-    for(int i = 0; i < N; i++){
-      cin >> p1 >> p2;
-      isDancing(p1, p2);
+    set<string> dancing;
+    dancing.insert("ChongChong");
+
+    string p1, p2;
+    for(int i = 0; i < N; i++) {
+        cin >> p1 >> p2;
+        if (dancing.count(p1) || dancing.count(p2)) {
+            dancing.insert(p1);
+            dancing.insert(p2);
+        }
     }
-    
-    cout << result << '\n';
+
+    cout << dancing.size() << '\n';
+
     return 0;
 }
