014dbc47fe3e3303427cf73081b56a58adc28f71
ssohy <109409651+ssohy@users.noreply.github.com>
2026-03-05 17:30:22 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-typedef unsigned long long ull;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ull n;
+    unsigned long long n;
     if (!(cin >> n)) return 0;
 
     if (n == 0) {
@@ -16,19 +14,17 @@ int main() {
         return 0;
     }
 
-    ull left = 1, right = 3037000500ULL; // sqrt(2^64) 근처값으로 제한
-    ull ans = right;
+    unsigned long long left = 1, right = n;
+    unsigned long long ans = right;
 
     while (left <= right) {
-        ull mid = left + (right - left) / 2;
+        unsigned long long mid = left + (right - left) / 2;
 
-        // mid * mid >= n 을 체크 (오버플로우 방지)
-        // mid가 충분히 커서 mid >= n / mid 이면 mid*mid >= n 일 가능성이 높음
         if (mid >= (n + mid - 1) / mid) { 
-            ans = mid;         // 조건 만족하는 q 후보 저장
-            right = mid - 1;   // 더 작은 q가 있는지 왼쪽 탐색
+            ans = mid;
+            right = mid - 1;
         } else {
-            left = mid + 1;    // 조건 만족 못하므로 오른쪽 탐색
+            left = mid + 1;
         }
     }
 
