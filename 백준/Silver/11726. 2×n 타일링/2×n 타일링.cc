14351079a65d5b7dcff954b053eb41819a662389
ssohy <109409651+ssohy@users.noreply.github.com>
2026-04-20 10:28:18 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,22 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n = 0, a = 1, b = 2; 
+
+    int n;
     cin >> n;
-    if (n == 1) { 
-      cout << 1 << '\n'; 
-      return 0; 
-      
-    }
+    vector<int> dp(n + 1);
+
+    dp[1] = 1;
+    if (n >= 2) dp[2] = 2;
 
     for (int i = 3; i <= n; i++) {
-        int c = (a + b) % 10007;
-        a = b; b = c;
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
     }
-    cout << b % 10007 << '\n';
+
+    cout << dp[n] << '\n';
     return 0;
 }
