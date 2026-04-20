61eb0ba11afe8a8e3b68b0648e3624b065921c46
ssohy <109409651+ssohy@users.noreply.github.com>
2026-04-20 10:21:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    const int MOD = 10007;
     int n = 0, a = 1, b = 2; 
-
     cin >> n;
     if (n == 1) { 
       cout << 1 << '\n'; 
@@ -14,10 +13,10 @@ int main() {
       
     }
 
-    for (int i = 3; i <= n; ++i) {
-        int c = (a + b) % MOD;
+    for (int i = 3; i <= n; i++) {
+        int c = (a + b) % 10007;
         a = b; b = c;
     }
-    cout << b % MOD << '\n';
+    cout << b % 10007 << '\n';
     return 0;
 }
