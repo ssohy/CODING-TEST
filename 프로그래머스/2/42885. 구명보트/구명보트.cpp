4031c35f2b87566ef8dc4ce2522605ca68482bed
ssohy <109409651+ssohy@users.noreply.github.com>
2026-04-21 16:31:50 +0900

    [level 2] Title: 구명보트, Time: 1.80 ms, Memory: 4.73 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,19 +1,18 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-    int answer = 0, sum = 0, left = 0, right = people.size() - 1;
+    int answer = 0, left = 0;
     sort(people.begin(), people.end());
     
-    while (left <= right) {
+    for (int right = people.size() - 1; left <= right; right--) {
         if (people[left] + people[right] <= limit) {
             left++;
         }
-        right--;
         answer++;
     }
+    
     return answer;
 }
